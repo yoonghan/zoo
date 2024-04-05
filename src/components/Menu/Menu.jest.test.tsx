3c1c483fea193518681cd7802fece4b0f381f429
Han <walcoorperation@gmail.com>
2024-04-05 14:20:13 +0800

    Add click checks for submenu.

--- src/components/Menu/Menu.jest.test.tsx
+++ src/components/Menu/Menu.jest.test.tsx
@@ -1,31 +1,42 @@
 import { render } from "@testing-library/react"
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 import { Menu } from "."
 
 describe("Menu", () => {
-  it("should display menu correctly", () => {
-    const {getByText, queryByText} = render(
-      <Menu
-        model={[
-            {
-                label: 'Furniture',
-                items: [
-                    [
-                        {
-                            label: 'Kitchen',
-                            items: [{ label: 'Bar stool' }, { label: 'Chair' }, { label: 'Table' }]
-                        }
-                    ]
-                ]
-            },
-            {
-                label: 'Electronics',
-            }
-        ]}
-      />
-    )
+
+  const renderMenuWithItems = () => render(
+    <Menu
+      model={[
+          {
+              label: 'Furniture',
+              items: [
+                  [
+                      {
+                          label: 'Kitchen',
+                          items: [{ label: 'Bar stool' }, { label: 'Chair' }, { label: 'Table' }]
+                      }
+                  ]
+              ]
+          },
+          {
+              label: 'Electronics',
+          }
+      ]}
+    />
+  )
+
+  it("should display main menu correctly", () => {
+    const {getByText, queryByText} = renderMenuWithItems()
     expect(getByText('Furniture')).toBeVisible()
     expect(getByText('Electronics')).toBeVisible()
     expect(queryByText('Kitchen')).not.toBeVisible()
+  })
+
+  it("should display sub menu correctly when main menu is clicked", async () => {
+    const {getByText} = renderMenuWithItems()
+    
+    await userEvent.click(getByText('Furniture'))
+    expect(getByText('Kitchen')).toBeVisible()
   })
 })
