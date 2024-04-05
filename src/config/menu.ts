6ba2f1736eb7b62f74ed5095bc6a981ce666f2f8
Han <walcoorperation@gmail.com>
2024-04-05 15:40:43 +0800

    Add menu

---
+++ src/config/menu.ts
@@ -0,0 +1,240 @@
+export const zooMenu = [
+    {
+        label: 'Zoo Negara',
+        url: '/about-us',
+        items: [
+            {
+                label: 'About Us',
+                url: '/about-us',
+            },
+            {
+                label: 'Zoo Negara Logo',
+                url: '/about-us#logo',
+            },
+            {
+                label: 'The 5 Pillars We Stand',
+                url: '/about-us#5pillars',
+            },
+            {
+                label: 'A Journey Through Time',
+                url: '/about-us#journeyThroughTime',
+            },
+            {
+                label: 'Careers',
+                url: '/about-us#careers',
+            }
+        ]
+    },
+    {
+        label: 'Visitor Info',
+        url: '/visitor',
+        items: [
+            {
+                label: 'Opening Hours And Rates',
+                url: '/visitor#opening-hours-and-rates',
+            },
+            {
+                label: 'Zoo Map',
+                url: '/visitor#zoo-map',
+            },
+            {
+                label: 'Getting There',
+                url: '/visitor#getting-there',
+            },
+            {
+                label: 'Food &amp; Soveniour Kiosk',
+                url: '/visitor#food-n-sovenior-kiosk',
+            },
+            {
+                label: 'Other Facilities',
+                url: '/visitor#other-facilities',
+            },
+            {
+                label: 'Show Times',
+                url: '/visitor#show-times',
+            },
+            {
+                label: 'Animal Feeding',
+                url: '/visitor#animal-feeding',
+            },
+            {
+                label: 'Rides',
+                url: '/visitor#rides',
+            },
+        ]
+    },
+    {
+        label: 'Have An Event',
+        url: '/event',
+        items: [
+            {
+                label: 'Birthday',
+                url: '/event#birthday',
+            },
+            {
+                label: 'Family Day',
+                url: '/event#family-day',
+            },
+            {
+                label: 'Scenic Point &amp; Function',
+                url: '/visitor#scenic-point-and-function',
+            },
+            {
+                label: 'Kancil Hall',
+                url: '/visitor#kancil-hall',
+            },
+            {
+                label: 'Tunku Abdul Rahman Theather',
+                url: '/visitor#tunku-abdul-rahman-theater',
+            }
+        ]
+    },
+    {
+        label: 'Get Involved',
+        url: '/get-involved',
+        items: [
+            {
+                label: 'Adopt Our Animals',
+                url: '/get-involved#adopt-our-animal',
+            },
+            {
+                label: 'List Of Donors And Sponsors',
+                url: '/get-involved#list-of-donors-and-sponsors',
+            },
+            {
+                label: 'KeeperKu Programme',
+                url: '/get-involved#keepersku-programme',
+            },
+            {
+                label: 'Student Training',
+                url: '/get-involved#student-training',
+            },
+            {
+                label: 'CSR Programme',
+                url: '/get-involved#csr-programme',
+            }
+        ]
+    },
+    {
+        label: 'News',
+        url: '/news',
+        items: [
+            {
+                label: 'Newspaper article',
+                url: '/news#newspaper-article',
+            },
+            {
+                label: 'Partnership News Highlight',
+                url: '/news#partnership-news-highlight',
+            },
+            {
+                label: 'Archive',
+                url: '/news#Archive',
+            },
+            {
+                label: 'Notice',
+                url: '/news#notice',
+            }
+        ]
+    },
+    {
+        label: 'Conservation',
+        url: '/conservation',
+        items: [
+            {
+                label: 'Animals in CITES',
+                url: '/conservation#animals-in-cites',
+            },
+            {
+                label: 'Research Papers On Milky Storks',
+                url: '/conservation#research-papers-on-milky-storks',
+            },
+            {
+                label: 'Research Papers On False Gahrial',
+                url: '/conservation#research-papers-on-false-gahrial',
+            },
+        ]
+    },
+    {
+        label: 'Education',
+        url: '/education'
+    },
+    {
+        label: 'Exhibits',
+        url: '/exhibits',
+        items: [
+            {
+                label: 'Giant Panda Conservation Centre',
+                url: '/exhibits#giant-panda-conservation-center',
+            },
+            {
+                label: 'Rimba Biodiversiti',
+                url: '/exhibits#rimba-biodiversiti',
+            },
+            {
+                label: 'Hornbill Centre',
+                url: '/exhibits#hornbill-centre',
+            },
+            {
+                label: 'Reptile House',
+                url: '/exhibits#reptile-house',
+            },
+            {
+                label: 'Malaysian Elephants',
+                url: '/exhibits#malaysian-elephants',
+            },
+            {
+                label: 'Lake Birds',
+                url: '/exhibits#lake-birds',
+            },
+            {
+                label: 'Bird Aviary',
+                url: '/exhibits#bird-aviary',
+            },
+            {
+                label: 'Australian Plain',
+                url: '/exhibits#australian-plain',
+            },
+            {
+                label: 'Humboldt Penguin',
+                url: '/exhibits#humboldt-penguin',
+            },
+            {
+                label: 'Children&apos;s World',
+                url: '/exhibits#childrens-world',
+            },
+            {
+                label: 'Bear Complex',
+                url: '/exhibits#bear-complex',
+            },
+            {
+                label: 'Savannah Walk',
+                url: '/exhibits#savannah-walk',
+            },
+            {
+                label: 'Hippopotamus',
+                url: '/exhibits#hippopotamus',
+            },
+            {
+                label: 'Mammal Kingdom',
+                url: '/exhibits#mamal-kingdom',
+            },
+            {
+                label: 'Tunku Abdul Rahman Aquarium',
+                url: '/exhibits#tunku-abdul-rahman-aquarium',
+            },
+            {
+                label: 'Cat Walk',
+                url: '/exhibits#cat-walk',
+            },
+            {
+                label: 'Ape Centre',
+                url: '/exhibits#ape-centre',
+            }
+        ]
+    },
+    {
+        label: 'Contact Us',
+        url: '/contact-us'
+    }
+]
