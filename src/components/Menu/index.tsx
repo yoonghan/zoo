372fa363b970b95161163e87451a95798ad1f6dd
Han <walcoorperation@gmail.com>
2024-04-05 14:38:51 +0800

    Reduce attribute supported.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,8 @@
 import { MegaMenu } from 'primereact/megamenu';
 import { MenuItem } from 'primereact/menuitem';
 
-export function Menu({model}: {model: MenuItem[]}) {
+
+
+export function Menu({model}: {model: Pick<MenuItem, "label" | "items" | "url" | "icon" | "separator">[]}) {
     return <MegaMenu model={model} breakpoint="960px" />
 }
