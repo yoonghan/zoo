9fbd5aefb8af5d62ff9a7688b16abf77e36bda5c
han <walcoorperation@gmail.com>
2024-04-07 19:28:41 +0800

    Add profile configuration.

---
+++ src/config/profile.ts
@@ -0,0 +1,36 @@
+import { FooterProps } from "@/components/Footer";
+
+export const zooProfile: FooterProps = {
+  companyName: "Zoo Negara",
+  operatingTime: {
+    day: { from: "Monday", to: "Sunday" },
+    time: { from: "9:00am", to: "5:00pm" },
+  },
+  address: {
+    street: "Zoo Negara",
+    city: "Hulu Kelang",
+    postalCode: 68000,
+    state: "Ampang",
+    country: "Selangor",
+  },
+  contact: {
+    phone: "For enquiries, call: +603-4108 3422/7/8, fax: +603-4107 5375.",
+  },
+  partners: [
+    {
+      imageSrc: "/partners/perhilitan.jpg",
+      url: "https://www.wildlife.gov.my",
+      alt: "Perhilitan Hutan",
+    },
+    {
+      imageSrc: "/partners/dbkl.jpg",
+      url: "https://www.dbkl.my",
+      alt: "DBKL",
+    },
+    {
+      imageSrc: "/partners/tanchong.jpg",
+      url: "https://www.tanchonggroup.com/",
+      alt: "Tan Chong Motors",
+    },
+  ],
+};
