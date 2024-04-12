d4a55431b915e994fae2a2d7cb26affc35ed3ecb
han <walcoorperation@gmail.com>
2024-04-12 23:16:05 +0800

    Fix button omit.

--- src/components/Button/Button.jest.test.tsx
+++ src/components/Button/Button.jest.test.tsx
@@ -23,7 +23,7 @@ describe("link", () => {
     className,
   }: {
     styling?: ButtonStyles["styling"];
-    className?: string
+    className?: string;
   }) =>
     render(
       <ButtonLink
@@ -36,14 +36,14 @@ describe("link", () => {
     );
 
   it("should render correct link className", () => {
-    const { getByRole } = renderLink({className: "external-className"});
+    const { getByRole } = renderLink({ className: "external-className" });
     const linkComponent = getByRole("button", { name: "I am a Link" });
     expect(linkComponent).toHaveClass("button-secondary");
     expect(linkComponent).toHaveClass("external-className");
   });
 
   it("should not render link with undefined", () => {
-    const { getByRole } = renderLink({className: undefined});
+    const { getByRole } = renderLink({});
     const linkComponent = getByRole("button", { name: "I am a Link" });
     expect(linkComponent).not.toHaveClass("undefined");
   });
