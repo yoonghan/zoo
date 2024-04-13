34a1e4b015842d889eb0ca7123c1ea1806d70e60
han <walcoorperation@gmail.com>
2024-04-13 19:46:36 +0800

    Auto check url to mark as external

--- src/components/Link/Link.jest.test.tsx
+++ src/components/Link/Link.jest.test.tsx
@@ -3,11 +3,17 @@ import "@testing-library/jest-dom";
 import { Link, type LinkStyles } from ".";
 
 describe("link", () => {
-  const renderLink = ({ styling, className }: { styling?: LinkStyles["styling"], className?: string }) =>
+  const renderLink = ({
+    styling,
+    className,
+  }: {
+    styling?: LinkStyles["styling"];
+    className?: string;
+  }) =>
     render(
       <Link
         styling={styling}
-        href="https://google.com"
+        href="https://www.google.com"
         className={className}
       >
         I am a Link
@@ -15,7 +21,7 @@ describe("link", () => {
     );
 
   it("should render correct link className", () => {
-    const { getByRole } = renderLink({className: "external-className"});
+    const { getByRole } = renderLink({ className: "external-className" });
     const linkComponent = getByRole("link", { name: "I am a Link" });
     expect(linkComponent).toHaveClass("link-primary");
     expect(linkComponent).toHaveClass("external-className");
@@ -36,7 +42,48 @@ describe("link", () => {
   });
 
   it("should render correct without adding undefined classname", () => {
-    const { getByRole } = renderLink({className: undefined});
-    expect(getByRole("link", { name: "I am a Link" })).not.toHaveClass("undefined");
+    const { getByRole } = renderLink({});
+    expect(getByRole("link", { name: "I am a Link" })).not.toHaveClass(
+      "undefined"
+    );
+  });
+
+  describe("rel attribute", () => {
+    const externalUrl = "https://www.google.com";
+
+    it("should render link with rel external if url starts with http", () => {
+      const { getByRole } = render(
+        <Link styling="Primary" href={externalUrl}>
+          I am a Link
+        </Link>
+      );
+      expect(getByRole("link", { name: "I am a Link" })).toHaveAttribute(
+        "rel",
+        "external"
+      );
+    });
+
+    it("should override users setup ref even if it's external", () => {
+      const { getByRole } = render(
+        <Link styling="Primary" href={externalUrl} rel="me">
+          I am a Link
+        </Link>
+      );
+      expect(getByRole("link", { name: "I am a Link" })).toHaveAttribute(
+        "rel",
+        "me"
+      );
+    });
+
+    it("should not send rel if url is internal", () => {
+      const { getByRole } = render(
+        <Link styling="Primary" href={"/localurl/back"}>
+          I am a Link
+        </Link>
+      );
+      expect(
+        getByRole("link", { name: "I am a Link" }).hasAttribute("rel")
+      ).toBeFalsy();
+    });
   });
 });
