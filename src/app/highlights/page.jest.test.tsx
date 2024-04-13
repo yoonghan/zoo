5577b15f89aabb1d5d947470a7d786e0fd2a5576
han <walcoorperation@gmail.com>
2024-04-14 07:22:16 +0800

    Add test cases for main page.

--- src/app/highlights/page.jest.test.tsx
+++ src/app/highlights/page.jest.test.tsx
@@ -1,32 +1,13 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import Career from "./page";
-import fs from "fs";
-import { zooProfile } from "@/config/profile";
-import { checkDownloadLinkHasHostAllLocalFiles } from "@/util/fileHelper";
+import Highlights from "./page";
 
-describe("Career", () => {
+describe("Highlights", () => {
   it("should contains important keys", () => {
-    const { getByRole } = render(<Career />);
+    const { getByRole } = render(<Highlights />);
     //main
     expect(getByRole("main")).toBeInTheDocument();
     //h1
-    expect(
-      getByRole("heading", { name: "Zoo Negara - Career" })
-    ).toBeInTheDocument();
-    //hr email
-    expect(
-      getByRole("link", { name: zooProfile.contactus.hrEmail })
-    ).toHaveAttribute("href", `mailto:${zooProfile.contactus.hrEmail}`);
-  });
-
-  it("should have valid local download links", () => {
-    const result = render(<Career />);
-    const allDownloads = checkDownloadLinkHasHostAllLocalFiles(
-      result.container
-    );
-    expect(allDownloads).toStrictEqual(
-      allDownloads.map((link) => ({ ...link, status: true }))
-    );
+    expect(getByRole("heading", { name: "Highlights" })).toBeInTheDocument();
   });
 });
--- src/app/highlights/page.tsx
+++ src/app/highlights/page.tsx
@@ -1,25 +1,13 @@
-import MiniMenu, { MiniMenuItems } from "@/components/MiniMenu";
-import { zooHighlights } from "@/config/highlights";
+import { MiniMenuItems } from "@/components/MiniMenu";
 import Image from "next/image";
-
-const miniLinks: MiniMenuItems[] = [
-  {
-    hashId: "jom-memancing",
-    title: "Jom Memancing",
-  },
-  {
-    hashId: "zoo-camping",
-    title: "Camping",
-  },
-];
 
 export default function Highlights() {
   return (
     <>
       <main>
         <h1 className="text-4xl font-bold text-center">Highlights</h1>
-        <article id={miniLinks[0].hashId}>
-          <h2 className="text-2xl font-bold mb-4">{miniLinks[0].title}</h2>
+        <article>
+          <h2 className="text-2xl font-bold mb-4">{"Jom Memancing"}</h2>
           <div className="flex flex-col md:flex-row">
             <Image
               src="/images/highlights/zoo-negara-fishing.jpg"
