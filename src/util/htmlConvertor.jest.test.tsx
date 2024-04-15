817d4f73ba35b94ee398cdb08e7fd5c7ad8d854f
Han <walcoorperation@gmail.com>
2024-04-15 12:56:41 +0800

    Add htmlConvertor to support links

--- src/util/htmlConvertor.jest.test.tsx
+++ src/util/htmlConvertor.jest.test.tsx
@@ -1,18 +1,45 @@
+import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import { htmlConvertor } from "./htmlConvertor";
 
 describe("HTML Convertor", () => {
-  describe("change astreick to bold", () => {
-    it("will render normal text correctly", () => {
-        expect(render(htmlConvertor("hello world")).baseElement.innerHTML).toBe('<div>hello world</div>')
-    });
+  it("will render normal text correctly", () => {
+      expect(render(htmlConvertor("hello world")).baseElement.innerHTML).toBe('<div>hello world</div>')
+  });
 
+  it("will replace all new lines with spaces", () => {
+    expect(render(htmlConvertor("hello\nworld")).baseElement.innerHTML).toBe('<div>hello world</div>')
+});
+
+  describe("change astrick to bold", () => {
     it("will render with bold correctly", () => {
         expect(render(htmlConvertor("hello *world")).baseElement.innerHTML).toBe('<div>hello <b>world</b></div>')
     });
 
-    it("will render not render an asterik in center", () => {
+    it("will not render a standalone asterik in center", () => {
         expect(render(htmlConvertor("hello * world")).baseElement.innerHTML).toBe('<div>hello * world</div>')
+    });
+  });
+
+  describe("change square bracket to anchor link", () => {
+    it("will render normal link correctly", () => {
+      const {getByRole} = render(htmlConvertor("[https://www.google.com|Google]"))
+      expect(getByRole("link", {name: "Google"})).toHaveAttribute("href", "https://www.google.com")
+      expect(getByRole("link", {name: "Google"})).toHaveAttribute("rel", "external")
+    });
+
+    it("will not render a standalone [ to link", () => {
+      expect(render(htmlConvertor("hello [link world")).baseElement.innerHTML).toBe('<div>hello [link world</div>')
+    });
+
+    it("will render text to link that is with space", () => {
+      const {getByRole} = render(htmlConvertor("[/visitors/about-us|About Us]"))
+      expect(getByRole("link", {name: "About Us"})).toHaveAttribute("href", "/visitors/about-us")
+    });
+
+    it("will render asterick text with | wrapped with link", () => {
+      const {getByRole} = render(htmlConvertor("hello [/link|*world i know is the best]"))
+      expect(getByRole("link", {name: "*world i know is the best"})).toHaveAttribute("href", "/link")
     });
   });
 });
--- src/util/htmlConvertor.tsx
+++ src/util/htmlConvertor.tsx
@@ -1,14 +1,50 @@
+import { Link } from "@/components/Link"
 import React from "react"
 
 export function htmlConvertor(text: string) {
-    const words = text.split(" ")
+    const unbreakableSpaceTextReplacement = "\n"
+
+    function substituteAnchorWithLink(word: string, parentKey: number) {
+        if(word.endsWith("]") && word.indexOf("|") > -1) {
+            const wordsWithNoBrackets = word.substring(1, word.length -1)
+            const linkWordPair = wordsWithNoBrackets.split("|")
+            const link = linkWordPair[0]
+            const text = linkWordPair[1].replaceAll(unbreakableSpaceTextReplacement, " ")
+            return <Link href={link} key={parentKey}>{text}</Link>
+        }
+        return word
+    }
+
+    const replaceWordsWithNewLineForSpecialTags = (text: string) => {
+        const wordsIdentifiedAsAnchor = text.split("|")
+        if(wordsIdentifiedAsAnchor.length > 1) {
+            return wordsIdentifiedAsAnchor.map(wordByPipe => {
+                const wordsAreAnchorEnded = wordByPipe.split("]")
+                if(wordsAreAnchorEnded.length === 2) {
+                    return `${wordsAreAnchorEnded[0].replaceAll(" ",unbreakableSpaceTextReplacement)}]${wordsAreAnchorEnded[1]}`
+                }
+                return wordByPipe
+            }).join("|")
+        }
+        return text
+    }
+
+    const words = replaceWordsWithNewLineForSpecialTags(text).split(" ")
     const boldedWords = words.map((word, idx) => {
-        if(word.startsWith("*") && word.length > 1) {
-            return <b key={idx}>{word.substring(1, word.length)}</b>
-        } else {
+        if(word.length < 2) {
             return word
         }
+
+        if(word.startsWith("*")) {    
+            return <b key={idx}>{word.substring(1, word.length)}</b>
+        } 
+        else if(word.startsWith("[")) { 
+            return substituteAnchorWithLink(word, idx)
+        } else {
+            return word.replaceAll(unbreakableSpaceTextReplacement, " ")
+        }
     })
+
     const joinWithSpace = boldedWords.reduce((prev, curr) => [...prev, ' ', curr], [] as (string|React.JSX.Element)[])
     joinWithSpace.shift() //remove first space
     return joinWithSpace
