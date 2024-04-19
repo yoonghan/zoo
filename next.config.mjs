636e6dd9df5cf098673d3496dd43b59c7101068f
Han <walcoorperation@gmail.com>
2024-04-19 12:08:02 +0800

    Restore nextjs, not possible to change

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,4 @@
  */
 const nextConfig = {}
 
-const staticConfig = {
-    reactStrictMode: true,
-    images: {
-      unoptimized: true,
-    },
-    output: "export"
-}
-
-export default process.env.NEXT_STATIC === "true" ? {...nextConfig, ...staticConfig} : nextConfig;
+export default nextConfig;
