3eb5e401bad1ecfa5ff7790d106a5ca742217268
han <walcoorperation@gmail.com>
2024-04-24 13:20:12 +0800

    Remove bundle analyzer, due to owasp.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
-import withBundleAnalyzer from "@next/bundle-analyzer";
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {};
 
-export default withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-})(nextConfig);
+export default nextConfig;
