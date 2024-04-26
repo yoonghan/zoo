e0db68023be1ddd9df75b0caeebcb8146cd84eb2
han <walcoorperation@gmail.com>
2024-04-26 16:23:34 +0800

    Add comment.

--- src/components/ScrollToTop/index.tsx
+++ src/components/ScrollToTop/index.tsx
@@ -1,7 +1,9 @@
-import dynamic from "next/dynamic"
+import dynamic from "next/dynamic";
+
+/** Unique to nodeJS, this informs the whole code is client side and will load after codes have been accessed. */
 
 const ScrollToTop = dynamic(() => import("./ScrollToTopNoSSR"), {
   ssr: false,
-})
+});
 
-export default ScrollToTop
+export default ScrollToTop;
