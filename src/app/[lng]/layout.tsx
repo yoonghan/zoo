12e0ec29569fb6e3f69a0c32d4037cd42169a4d7
han <mailyoonghan@gmail.com>
2025-06-24 13:09:08 +0800

    Add language switcher

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "@/themes/lara-light-green/theme.css";
-import "./main.css";
+import "../main.css";
 import { Announcement } from "@/components/Announcement";
 import { Footer } from "@/components/Footer";
 import { zooProfile } from "@/config/profile";
@@ -14,6 +14,7 @@ import { dir } from 'i18next'
 import { languages } from '../../i18n/settings'
 import { Usable, use } from 'react'
 import { PageParams } from "@/typings/params";
+import Link from "next/link";
 
 export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }))
@@ -46,20 +47,30 @@ export default function RootLayout({
           />
           <Menu
             model={zooMenu}
-            mobileHomeText="Zoo Negara Malaysia"
+            mobileHomeText="Zoo Negara"
             language={lng}
             shortcutComponent={
-              <ButtonLink
-                styling="BuyNow"
-                href={zooProfile.ticket.admission.url}
-              >
-                <FontAwesomeIcon
-                  icon={faTicket}
-                  className="inline mr-2"
-                  width={20}
-                />
-                {zooProfile.ticket.admission.text}
-              </ButtonLink>
+              <form>
+                {languages.map((language, idx) => (
+                  <span key={language} className={language === lng ? "hidden sm:inline" : ""}>
+                    {idx !== 1 || <span className={"hidden sm:inline -mx-2"}> | </span>}
+                    <Link href={`/${language}`} className={language === lng ? "hidden sm:inline underline mx-4" : "mx-4"}> 
+                      {language.toUpperCase()}
+                    </Link>
+                  </span>
+                ))}
+                <ButtonLink
+                  styling="BuyNow"
+                  href={zooProfile.ticket.admission.url}
+                >
+                  <FontAwesomeIcon
+                    icon={faTicket}
+                    className="inline mr-2"
+                    width={20}
+                  />
+                  <span className="hidden sm:inline">{zooProfile.ticket.admission.text}</span>
+                </ButtonLink>
+              </form>
             }
           />
         </header>
