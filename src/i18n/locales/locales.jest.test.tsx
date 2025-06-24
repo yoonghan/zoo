1e89551d6912f26cd32d993a611bb0b2ead4b860
han <mailyoonghan@gmail.com>
2025-06-24 08:10:04 +0800

    Add test for announcement and faq locale.

--- src/i18n/locales/locales.jest.test.tsx
+++ src/i18n/locales/locales.jest.test.tsx
@@ -1,10 +1,24 @@
 import bm from "./bm/translation.json";
 import en from "./en/translation.json";
 
+import zooAnnouncementEN from "./en/announcements";
+import zooAnnouncementBM from "./bm/announcements";
+
+import zooFaqEN from "./en/faq";
+import zooFaqBM from "./bm/faq";
+
 describe("i18n locales", () => {
   it("all bm should have all en fields", () => {
     const enKeys = Object.keys(en);
     const bmKeys = Object.keys(bm);
     expect(bmKeys).toEqual(expect.arrayContaining(enKeys));
   });
+
+  it("all announcement should be defined in both languages", () => {
+    expect(zooAnnouncementBM.length).toBe(zooAnnouncementEN.length);
+  })
+
+  it("all faq should be defined in both languages", () => {
+    expect(zooFaqEN.length).toBe(zooFaqBM.length);
+  })
 });
