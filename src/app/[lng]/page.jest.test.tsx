76af28327fa1b0938d898e731ea0a57b565627a5
han <mailyoonghan@gmail.com>
2025-06-29 08:30:30 +0800

    Remove unused suspense.

--- src/app/[lng]/page.jest.test.tsx
+++ src/app/[lng]/page.jest.test.tsx
@@ -1,6 +1,5 @@
 import Home from "./page";
 import { render, screen, act } from "@testing-library/react";
-import { Suspense } from "react";
 
 /** Testing is limited for layout, as it renders whole html. The only best solution is via e2e test. */
 describe("Main Homepage", () => {
