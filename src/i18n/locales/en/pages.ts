2f0381c46f0fe82e0e6933d68bcad95bbd952733
Yoong Han <walcoorperation@gmail.com>
2025-07-03 07:32:28 +0800

    Update about us (#166)

--- src/i18n/locales/en/pages.ts
+++ src/i18n/locales/en/pages.ts
@@ -2,45 +2,49 @@ const translations = {
   lang: "en",
   headers: {
     default: "Zoo Negara Malaysia",
-    defaultDescription: "A non-governmental organization established to create the first local zoo for Malaysians."
+    defaultDescription:
+      "A non-governmental organization established to create the first local zoo for Malaysians.",
   },
   homepage: {
-    "welcome": "Welcome to Zoo Negara",
-    "introduction": "Zoo Negara Malaysia is managed by the Malaysian Zoological Society, a non-governmental organization established to create the first local zoo for Malaysians. Zoo Negara was officially opened on 14th November 1963 and has matured into a well-known zoo all around the world. We have a total of over 5137 specimen from 476 species of mammals, birds, reptiles, amphibians and fish.",
-    "introduction-remark": "taken from Zoo Negara website"
+    welcome: "Welcome to Zoo Negara",
+    introduction:
+      "Zoo Negara Malaysia is managed by the Malaysian Zoological Society, a non-governmental organization established to create the first local zoo for Malaysians. Zoo Negara was officially opened on 14th November 1963 and has matured into a well-known zoo all around the world. We have a total of over 5137 specimen from 476 species of mammals, birds, reptiles, amphibians and fish.",
+    "introduction-remark": "taken from Zoo Negara website",
   },
   aboutUs: {
-    "title": "About Us",
+    title: "About Us",
     aboutWalcron: {
       title: "About Walcron",
-      description: "We are a couple who have passion and interest in building sustainable websites. Besides, we do love to go to the zoo!",
-      desription2: "The reason we created a non-associated and alternate website about Zoo Negara Malaysia are mainly due to the lack of basic Website supports. We did try to reach out to the Zoo admin but we got rejected politely.",
+      description:
+        "We are a couple who have passion and interest in building sustainable websites. Besides, we do love to go to the zoo!",
+      desription2:
+        "The reason we created a non-associated and alternate website about Zoo Negara Malaysia are due to the lack of basic Website support in the original site. We did try to reach out to the Zoo admin on this site, but our proposal was rejected politely. This zoo website add-ons:",
       descriptionSupport: [
-        "Lack of Language Support, BM is the national language.",
-        "No ARIA/disable support.",
-        "Annoying Pop-up in main page.",
-        "The site does not get update.",
-        "Old style.",
-        "Missing Sitemap."
-      ]
+        "Language Support as Malaysian BM as the national language is required.",
+        "Supports ARIA for users with disabilities.",
+        "No Annoying Pop-up in main page.",
+        "Add Missing Sitemap.",
+      ],
     },
     aboutZoo: {
       title: "About Zoo Negara Malaysia",
-      description: "Zoo Negara Malaysia is managed by the Malaysian Zoological Society, a non-governmental organization established to create the first local zoo for Malaysians.",
-      descriptionWiki: "https://en.wikipedia.org/wiki/National_Zoo_of_Malaysia"
+      description:
+        "Zoo Negara Malaysia is managed by the Malaysian Zoological Society, a non-governmental organization established to create the first local zoo for Malaysians.",
+      descriptionWiki: "https://en.wikipedia.org/wiki/National_Zoo_of_Malaysia",
     },
     vision: {
       title: "Vision",
-      description: "We wish Zoo Negara decides to absorb this website and update the website to fit their needs. The site can be self-maintained as basic DevOps infrastructure has been built around it.",
+      description:
+        "We wish Zoo Negara decides to absorb this website and update the website to fit their needs. The site can be self-maintained as basic DevOps infrastructure has been built around it.",
       descriptionSupport: [
         "Site builds, tests and deploy by itself.",
         "Minimal basic JSON/HTML is required to maintain it.",
         "Everything is integrated with Github pipeline.",
         "Opensourced - anyone can view and contribute.",
-        "Process oriented - approval/reviews before accepting changes."
-      ]
-    }
-  }
-}
+        "Process oriented - approval/reviews before accepting changes.",
+      ],
+    },
+  },
+};
 
-export default translations
+export default translations;
