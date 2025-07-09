b78d04a59567fd2852c89c2fe90f2a5e88e3e6f8
han <walcoorperation@gmail.com>
2025-07-10 06:02:05 +0800

    Fix snapshot run with localhost.

--- backstop_data/engine_scripts/playwright/onBefore.js
+++ backstop_data/engine_scripts/playwright/onBefore.js
@@ -1,5 +1,11 @@
-module.exports = async (page, scenario, viewport, isReference, browserContext) => {
+module.exports = async (
+  page,
+  scenario,
+  viewport,
+  isReference,
+  browserContext
+) => {
   //await require('./loadCookies')(browserContext, scenario);
-  //await require('./interceptImages')(page, scenario);
-  await require('./interceptIframe')(page, scenario);
+  await require("./interceptImages")(page, scenario);
+  await require("./interceptIframe")(page, scenario);
 };
