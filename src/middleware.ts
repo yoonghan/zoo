076e7c33c26bd37444dc0efd7108bad8c231a6ff
han <walcoorperation@gmail.com>
2025-07-12 02:24:27 +0800

    Add not found page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,48 @@
-import { NextResponse } from 'next/server'
-import acceptLanguage from 'accept-language'
-import { fallbackLng, languages, cookieName } from './i18n/settings'
+import { NextResponse } from "next/server";
+import acceptLanguage from "accept-language";
+import { fallbackLng, languages, cookieName } from "./i18n/settings";
 
-acceptLanguage.languages(languages)
+acceptLanguage.languages(languages);
 
 export const config = {
-  // matcher: '/:lng*'
-  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)']
+  matcher: [
+    "/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)",
+  ],
+};
+
+function getLanguage(req: any) {
+  if (req.cookies.has(cookieName))
+    return acceptLanguage.get(req.cookies.get(cookieName).value);
+  if (req.headers.get("Accept-Language"))
+    return acceptLanguage.get(req.headers.get("Accept-Language"));
+
+  return fallbackLng;
 }
 
-export function middleware(req:any) {
-  let lng
-  if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName).value)
-  if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'))
-  if (!lng) lng = fallbackLng
+export function middleware(req: any) {
+  let lng = getLanguage(req);
 
   // Redirect if lng in path is not supported
   if (
-    !languages.some(loc => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
-    !req.nextUrl.pathname.startsWith('/_next') && 
-    !req.nextUrl.pathname.startsWith('/images') &&
-    !req.nextUrl.pathname.startsWith('/sitemap.xml')
+    !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
+    !req.nextUrl.pathname.startsWith("/_next") &&
+    !req.nextUrl.pathname.startsWith("/images") &&
+    !req.nextUrl.pathname.startsWith("/sitemap.xml")
   ) {
-    return NextResponse.redirect(new URL(`/${lng}${req.nextUrl.pathname}`, req.url))
+    return NextResponse.redirect(
+      new URL(`/${lng}${req.nextUrl.pathname}`, req.url)
+    );
   }
 
-  if (req.headers.has('referer')) {
-    const refererUrl = new URL(req.headers.get('referer'))
-    const lngInReferer = languages.find((l) => refererUrl.pathname.startsWith(`/${l}`))
-    const response = NextResponse.next()
-    if (lngInReferer) response.cookies.set(cookieName, lngInReferer)
-    return response
+  if (req.headers.has("referer")) {
+    const refererUrl = new URL(req.headers.get("referer"));
+    const lngInReferer = languages.find((l) =>
+      refererUrl.pathname.startsWith(`/${l}`)
+    );
+    const response = NextResponse.next();
+    if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
+    return response;
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
