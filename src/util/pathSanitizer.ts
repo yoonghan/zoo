4f54b4c9de8e2973b39146dbfae60408f1a46d3f
Yoong Han <walcoorperation@gmail.com>
2025-07-14 13:50:39 +0800

    Update to test testcoverage on pathSanitizer.ts

--- src/util/pathSanitizer.ts
+++ src/util/pathSanitizer.ts
@@ -1,6 +1,8 @@
 
 export const sanitizePath = (path: string) => {
   // Ensure the path is sanitized to prevent XSS attacks
-  /* v8 ignore next */
-  return path.startsWith("/") ? path : `/${path}`
+  if (path.startsWith("/")) {
+    return path
+  }
+  return `/${path}`
 }
