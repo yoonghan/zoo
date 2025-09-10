3bcbadc0316831516c1157825ef700cde370c2b7
han <mailyoonghan@gmail.com>
2025-09-11 07:26:47 +0800

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/themes/lara-light-green/theme.css";
 import "./main.css";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -9,7 +10,29 @@ export default function RootLayout({
   return (
     <html lang={"en"}>
       <head>
-        <meta http-equiv="Content-Security-Policy" content="default-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self'; connect-src 'self';"></meta>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="
+            default-src 'self' 'unsafe-inline';
+            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com;
+            style-src 'self' 'unsafe-inline';
+            img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com;
+            connect-src 'self' https://www.google-analytics.com https://analytics.google.com;
+          "
+        ></meta>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4GRBN4E8PX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-4GRBN4E8PX');
+          `}
+        </Script>
       </head>
       <body>{children}</body>
     </html>
